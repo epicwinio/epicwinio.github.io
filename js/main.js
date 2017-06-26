b7970b6ed8a665f75a72424b2ae22045167206c5
Chris Jacob <i.chris.jacob@gmail.com>
2017-06-27 00:08:47 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $( document ).ready(function() {
   $(function () {
     $(window).scroll(function () {
       // set distance user needs to scroll before we start fadeIn
-      if ($(this).scrollTop() > 350) {
+      if ($(this).scrollTop() > 280) { // 350 - 70 = 280
         $('body').addClass('show-navbar');
       } else {
         $('body').removeClass('show-navbar');
