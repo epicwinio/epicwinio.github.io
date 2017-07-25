d25880dbe17dfcbd099a39d1abdfa6b76eb9f5ee
Chris Jacob <i.chris.jacob@gmail.com>
2017-07-26 00:45:43 +1000

    description

--- assets/main.js
+++ assets/main.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
   $(function () {
     $(window).scroll(function () {
       // set distance user needs to scroll before we start fadeIn
-      if ($(this).scrollTop() > 280) { // 350 - 70 = 280
+      if ($(this).scrollTop() > 350) { // 350 - 70 = 280
         $('body').addClass('show-navbar');
       } else {
         $('body').removeClass('show-navbar');
